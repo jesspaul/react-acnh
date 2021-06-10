328a1bbcd89c376e75e989a5ed1502d8e479c343
Jessica Paul <paul.jessica95@gmail.com>
2021-06-10 18:02:57 -0400

    name category page

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,9 +1,13 @@
 import './Category.css';
+import { useContext } from 'react';
+import { AcnhContext } from '../../contexts/acnhapi';
 
 const Category = () => {
+    const { branch } = useContext(AcnhContext);
+
     return (
         <div className="Category">
-            Category Page
+            Category Page for {branch}
         </div>
     );
 }
