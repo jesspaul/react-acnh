12e48a032260bc5d8b452a14943c5b55cb49ed7b
Jessica Paul <paul.jessica95@gmail.com>
2021-06-10 16:48:53 -0400

    home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,11 @@
+import poster from '../../img/acnh-poster.jpg';
+import './Home.css';
+
 const Home = () => {
     return (
         <div className="Home">
-            Home
+            <h1>Animal Crossing: New Horizons</h1>
+            <img id="poster" src={poster} />
         </div>
     );
 }
