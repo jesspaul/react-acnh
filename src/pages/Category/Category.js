439245ef319c5044546972e2ed0732eef6021f20
Jessica Paul <paul.jessica95@gmail.com>
2021-06-10 17:04:42 -0400

    add category page

---
+++ src/pages/Category/Category.js
@@ -0,0 +1,11 @@
+import './Category.css';
+
+const Category = () => {
+    return (
+        <div className="Category">
+            Category Page
+        </div>
+    );
+}
+ 
+export default Category;
